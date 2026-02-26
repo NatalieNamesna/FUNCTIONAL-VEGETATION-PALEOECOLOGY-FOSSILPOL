4a127008c052353ec6392e7a91fe51efffa2cb5c
Natalie Namesna <hanusova.nat@gmail.com>
2026-02-26 09:53:39 +0100

    editing harmonisation scripts

--- R/01_Data_processing/05_Harmonisation/01_Harmonisation.R
+++ R/01_Data_processing/05_Harmonisation/01_Harmonisation.R
@@ -44,7 +44,20 @@ data_with_chronologies <-
       data_storage_path, # [config_criteria]
       "/Data/Processed/Data_with_chronologies"
     )
+  ) %>% 
+  dplyr::mutate(
+    harmonisation_region = dplyr::case_when(
+      .default = harmonisation_region,
+      harmonisation_region == "New Zealand" ~ "IndoPacific",
+      harmonisation_region == "IndoPacific_Islands" ~ "IndoPacific",
+      harmonisation_region == "Papua_New_Guinea" ~ "IndoPacific",
+      harmonisation_region == "Tasmania_and_islands" ~ "IndoPacific",
+      harmonisation_region == "Australia_Southeast" ~ "IndoPacific",
+      harmonisation_region == "Australia_North" ~ "IndoPacific",
+    )
   )
+
+
 
 # test the presence of data
 RUtilpol::check_if_loaded(
