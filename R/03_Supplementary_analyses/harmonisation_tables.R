2670318da7388c605d59f11299233b5cec097f5e
Natalie Namesna <hanusova.nat@gmail.com>
2026-02-26 15:32:40 +0100

    edit

--- R/03_Supplementary_analyses/harmonisation_tables.R
+++ R/03_Supplementary_analyses/harmonisation_tables.R
@@ -52,6 +52,96 @@ unique_Birks_Latin_America <- Birks_Latin_America %>%
 unique_Birks_North_America <- Birks_North_America %>% 
   distinct(taxon_name, .keep_all = TRUE)
 
+#----------------------------------------------------------#
+# 3. Load each empty table -----
+#----------------------------------------------------------#
+
+empty_Asia_Levant <- read.csv("Data/Input/Harmonisation_tables/Asia_Levant_2026-02-24.csv")
+empty_Asia_Main <- read.csv("Data/Input/Harmonisation_tables/Asia_Main_2026-02-24.csv")
+empty_Asia_Siberia <- read.csv("Data/Input/Harmonisation_tables/Asia_Siberia_2026-02-24.csv")
+empty_Europe <- read.csv("Data/Input/Harmonisation_tables/Europe_2026-02-24.csv")
+empty_Indopacific <- read.csv("Data/Input/Harmonisation_tables/IndoPacific_2026-02-24.csv")
+empty_Latin_America <- read.csv("Data/Input/Harmonisation_tables/Latin_America_2026-02-24.csv")
+empty_North_America <- read.csv("Data/Input/Harmonisation_tables/North_America_2026-02-24.csv")
+
+
+#----------------------------------------------------------#
+# 4. Join Birks tables and empty tables by taxon_name -----
+#----------------------------------------------------------#
+
+# Asia Levant
+joined_empty_Birks_tables_Asia_Levant <-
+  left_join(empty_Asia_Levant, Birks_Asia_Levant, by = "taxon_name")
+
+# Asia Main
+joined_empty_Birks_tables_Asia_Main <-
+  left_join(empty_Asia_Main, Birks_Asia_Main, by = "taxon_name")
+
+# Asia Siberia
+joined_empty_Birks_tables_Asia_Siberia <-
+  left_join(empty_Asia_Siberia, Birks_Asia_Siberia, by = "taxon_name")
+
+# Europe
+joined_empty_Birks_tables_Europe <-
+  left_join(empty_Europe, Birks_Europe, by = "taxon_name")
+
+# Indopacific
+joined_empty_Birks_tables_Indospecific <-
+  left_join(empty_Indopacific, Birks_Indopacific, by = "taxon_name")
+
+# Latin America
+joined_empty_Birks_tables_Latin_America <-
+  left_join(empty_Latin_America, Birks_Latin_America, by = "taxon_name")
+
+# North America
+joined_empty_Birks_tables_North_America <-
+  left_join(empty_North_America, Birks_North_America, by = "taxon_name")
+
+
+#----------------------------------------------------------#
+# 5. Make a list of taxons, which are not present in Birks -----
+#----------------------------------------------------------#
+
+# Asian Levant
+
+
+# Asia Main
+
+
+# Asia Siberia
+
+
+# Europe
+
+
+# Indopacific
+
+
+# Latin America
+
+
+# North America
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
